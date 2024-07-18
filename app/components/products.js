ea206ba97b9bec76ed0ca53648f8f09334bb43cc
guzamak <120812215+guzamak@users.noreply.github.com>
2024-07-18 18:43:20 +0700

    Update products.js

--- app/components/products.js
+++ app/components/products.js
@@ -5,8 +5,8 @@ export default function Products({ title, productArr }) {
       <div className="w-[1239px] flex flex-col items-center space-y-14">
         <h1 className="text-[48px] font-DM font-semibold">{title}</h1>
         <div className="inline-flex w-full justify-between">
-          {productArr.map(product => (
-            <Product name={product.name} star={product.star} price={product.price} discount={product.discount} image={product.image}/>
+          {productArr.map((product,index) => (
+            <Product name={product.name} star={product.star} price={product.price} discount={product.discount} image={product.image} key={index}/>
           ))}
         </div>
         <button className=" w-[218px] h-[52px] border-[1px] rounded-[62px] text-[16px] font-inter">View All</button>
